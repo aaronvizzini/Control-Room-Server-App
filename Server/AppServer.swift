da930a45149a9653b73532d8db10ba2d74684d88
Aaron Vizzini <aaronvizzini@gmail.com>
2017-07-31 19:59:39 -0400

    Comment change

--- Server/AppServer.swift
+++ Server/AppServer.swift
@@ -177,7 +177,7 @@ class AppServer: NSObject, GCDAsyncSocketDelegate {
             }
             
             //@synchronized(connectedSockets)
-            // {
+            // { 
             connectedSockets.remove(at: connectedSockets.index(of: sock)!)
             // }
         }
